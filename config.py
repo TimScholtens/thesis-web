5d8f793b321144be447b2377d23dda69c17ddae5
Tim Scholtens <t.p.h.scholtens@student.tue.nl>
2020-12-22 22:14:54 +0100

    init

---
+++ config.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+STATIC_URL_PATH = ''  # Removes 'static' from URL
+STATIC_FOLDER = 'src/'
+
+app = Flask(__name__, static_url_path=STATIC_URL_PATH, static_folder=STATIC_FOLDER)
