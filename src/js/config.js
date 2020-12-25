51330e07f07d33f3ddde8a8aabc21baf0b0359d5
Tim <timscholtens@hotmail.com>
2020-12-25 08:47:36 +0100

    added neighbourhoods geojson

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,10 @@
+// API
 let HOST = 'http://localhost:5000'
 
-export {HOST}
+
+// Map
+let START_COORDINATES = {lat: '52.2702157', long: '4.895167899999933'}
+let START_ZOOM = '8'
+let MIN_ZOOM = '8'
+
+export {HOST, START_COORDINATES, START_ZOOM, MIN_ZOOM}
--- src/js/map.js
+++ src/js/map.js
@@ -1,21 +1,35 @@
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css';
+import {START_COORDINATES, START_ZOOM, MIN_ZOOM} from './config.js'
+import * as neighbourhood_polygons from '../geojson/neighbourhoods.json'
 
 
+function init(){
+	let map = L.map('map').setView([START_COORDINATES['lat'],START_COORDINATES['long'] ], START_ZOOM);
 
-let map = L.map('map').setView([39.74739, -105], 13);
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		minZoom: MIN_ZOOM,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/light-v9',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(map);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	id: 'mapbox/light-v9',
-	tileSize: 512,
-	zoomOffset: -1
-}).addTo(map);
+	return map
+
+}
+
+function loadNeighbourhoods(map){
+	L.geoJSON(data)
+}
 
 
 function getSelectedNeighbourhoods(){
 
 }
+
+let map = init()
+
+loadNeighbourhoods(map)
