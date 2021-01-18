8296717c8565dd3c18e3f342236033eb6a5b67d7
Tim Scholtens <t.p.h.scholtens@student.tue.nl>
2021-01-18 18:32:10 +0100

    init

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,7 @@
-import '../css/style.scss'
-import './map.js'
-import './toolbar.js'
-import 'materialize-css';
-import 'materialize-css/dist/css/materialize.min.css';
+// import '../css/style.scss'
+// import './map.js'
+// import './toolbar.js'
+// import 'materialize-css';
+// import 'materialize-css/dist/css/materialize.min.css';
+
+console.log('boiii')
--- src/frontend/webpack.common.js
+++ src/frontend/webpack.common.js
@@ -1,11 +1,19 @@
- const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
- module.exports = {
-   entry: {
-     app: './app.js',
-   },
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
- };
+module.exports = {
+    entry: {
+        app: './app.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: './index.html' ,
+            template: "./static/html/index.html"
+        })
+    ]
+};
