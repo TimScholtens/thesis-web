00dffaf078812a8eb13c0918c093dd541cc15ebc
Tim Scholtens <t.p.h.scholtens@student.tue.nl>
2021-01-21 16:46:15 +0100

    quick rework frontend

---
+++ src/frontend/static/js/slider.js
@@ -0,0 +1,20 @@
+import wNumb from 'wnumb'
+import noUiSlider from 'nouislider';
+import 'nouislider/distribute/nouislider.css';
+
+
+let slider = document.getElementById('year-slider');
+noUiSlider.create(slider, {
+    start: [2000, 2020],
+    connect: true,
+    step: 1,
+    orientation: 'horizontal', // 'horizontal' or 'vertical'
+    range: {
+        'min': 2000,
+        'max': 2020
+    },
+    format: wNumb({
+        decimals: 0
+    })
+});
+      
